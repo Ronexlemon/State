716d45a9550fa8294da69d1eb7bcd72f77181718
RonexLemon <ronexondimu@gmail.com>
2024-12-08 22:01:52 +0300

    mul state

--- src/state/counter/counterSlice.ts
+++ src/state/counter/counterSlice.ts
@@ -3,13 +3,7 @@ import { createSlice, PayloadAction } from "@reduxjs/toolkit"
 interface counterState{
     value: number
 }
-interface multiplicationSate{
-    value:number
-}
 
-const initialMultiplication:multiplicationSate={
-    value:10
-}
 
 const initialState:counterState ={
     value:0
@@ -31,24 +25,8 @@ const counterSlice = createSlice({
     }
 })
 
-const multiplication = createSlice({
-    name: 'multiplication',
-    initialState:initialMultiplication,
-    reducers: {
-        times5:(state)=>{
-            state.value = state.value *5
-        },
-        times10:(state)=>{
-            state.value = state.value *10
-        },
-        timesAmount:(state,action:PayloadAction<number>)=>{
-            state.value = state.value * action.payload
 
-        }
-    }
-})
 
 export  const{increment,decrement,incrementByAmount} = counterSlice.actions
-
 
 export default counterSlice.reducer;
---
+++ src/state/mul/multiplicationSlice.ts
@@ -0,0 +1,26 @@
+import { createSlice,PayloadAction } from "@reduxjs/toolkit";interface multiplicationSate{
+    value:number
+}
+
+const initialMultiplication:multiplicationSate={
+    value:10
+}
+const multiplication = createSlice({
+    name: 'multiplication',
+    initialState:initialMultiplication,
+    reducers: {
+        times5:(state)=>{
+            state.value = state.value *5
+        },
+        times10:(state)=>{
+            state.value = state.value *10
+        },
+        timesAmount:(state,action:PayloadAction<number>)=>{
+            state.value = state.value * action.payload
+
+        }
+    }
+})
+export const{times10,times5,timesAmount} = multiplication.actions;
+
+export default multiplication.reducer
